140c7114f7eae13ddcf4b1d7747904e3237d77c9
Haris Gillani <haris.gillani@factset.com>
2021-05-09 13:23:59 +0100

    -added user type

--- PersonalSite/ClientApp/src/app/auth/authGuard.ts
+++ PersonalSite/ClientApp/src/app/auth/authGuard.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild, Router } from '@angular/router';
-import { AuthService } from 'src/services/auth.service';
-import { UserDTO } from 'src/dto/userDTO';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { User } from '../model/user';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) { }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user: User = JSON.parse(localStorage.getItem('user'));
 
     if (user != null) {
       return true;
