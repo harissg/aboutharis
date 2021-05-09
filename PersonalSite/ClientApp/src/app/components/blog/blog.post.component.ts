deb978866c49bdd45cdcb48184649efee6928186
Haris Gillani <haris.gillani@factset.com>
2021-05-09 13:26:42 +0100

    - added isEditable post button - added auth guard check local storage if true enable edit button - added editpost func

--- PersonalSite/ClientApp/src/app/components/blog/blog.post.component.ts
+++ PersonalSite/ClientApp/src/app/components/blog/blog.post.component.ts
@@ -2,9 +2,11 @@
 
 import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { Post } from 'src/app/model/post';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { PostService } from 'src/services/post.service';
 import { Observable } from 'rxjs';
+import { UserDTO } from 'src/dto/userDTO';
+import { AuthGuard } from 'src/app/auth/authGuard';
 
 @Component({
   selector: 'app-blogs-component',
@@ -13,13 +15,23 @@ import { Observable } from 'rxjs';
 export class BlogPostComponent implements OnInit {
   public post: Post;
   public post$: Observable<Post>;
+  public user: UserDTO;
+  public isEditable: boolean = false;
+
   @ViewChild('content', { static: true }) div: ElementRef;
 
   constructor(private renderer: Renderer2, private activeRoute: ActivatedRoute,
-    private postService: PostService, private router: Router) {
+    private postService: PostService, private router: Router, private authGuard: AuthGuard) {
   }
 
   ngOnInit() {
+    // so we can edit posts
+    let user = localStorage.getItem('user');
+    if (user) {
+      this.isEditable = true;
+    } else {
+      this.isEditable = false;
+    }
     this.post = new Post();
     const postId = this.activeRoute.snapshot.queryParams['id'];
     this.post$ = this.postService.getById(postId);
@@ -27,6 +39,7 @@ export class BlogPostComponent implements OnInit {
       this.post.title = res.title;
       this.post.content = res.content;
       this.post.comments = res.comments;
+      this.post.postId = res.postId;
       this.formatContent(this.post.content); // for now only 1
     });
   }
@@ -37,4 +50,10 @@ export class BlogPostComponent implements OnInit {
     this.renderer.appendChild(this.div.nativeElement, div);
   }
 
+  editPost(id: string) {
+    const navigationExtras: NavigationExtras = {
+      queryParams: { 'id': id }
+    };
+    this.router.navigate(['edit'], navigationExtras);
+  }
 }
